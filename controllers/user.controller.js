222e26660888677a1ccf5896a746a0f3be9177ac
Gideon ojo <44063695+Gideon7@users.noreply.github.com>
2021-11-17 11:23:34 +0100

    updated endpoints

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,140 @@
-const user = require('../models/UserSchema')
-const { NODE_ENV } = process.env;
-
-const addUser = (req, res) => {
-    if(!req.body.name){
-        
+const bcrypt = require('bcrypt')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+      host : 'localhost',
+      port : 5432,
+      user : 'postgres',
+      password : 'test',
+      database : 'TestDB'
     }
+  });
+
+const addUser = (req,res) => {
+    //grab body from request and push to database
+    const saltRounds = 10;
+    const {name, email, password} = req.body;
+    if (!name || !email || !password){
+       return res.status(400).json("Fill Up Empty Textfields")
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    createdDate: new Date().toISOString()
+                }).then(user => {
+                    res.json(user[0])
+                })
+        })
+        .then(trx.commit)
+        .then(trx.rollback)
+    })
+    .catch(err => res.status(400).json("unable to add to database"))
+
+}
+
+const signin = (req, res) => {
+    const {email, password} = req.body
+    if (!email || !password){
+        return res.status(400).json("Fill Up Empty Textfields")
+     }
+    db.select('email', 'hash').from('login')
+         .where('email', '=', email)
+         .then(data => {
+             const isValid = bcrypt.compareSync(password, data[0].hash)
+             if (isValid){
+                return db.select('*').from('users')
+                     .where('email', '=', email)
+                     .then(user => {
+                         res.status(200).json(user[0])
+                     })
+                     .catch(err => res.status(400).json("unable to get user"))
+             }else{
+                  res.status(400).json("Wrong credentials")
+             }
+         })
+         .catch(err => res.status(400).json("Wrong credentials"))
+ }
+
+ const getUserProfile = (req, res) => {
+    const { userID } = req.params;
+    db.select('*').from('users')
+    .where({id: userID})
+    .then(user => {
+        if (user.length){
+            res.json(user[0])
+        }else{
+            res.status(400).json('User Not Found')
+        }
+    })
+    .catch(err => res.status(400).json('Database disrupt error'))
+    
+}
+
+const addEntries = (req,res) => {
+    const { id } = req.body;
+    console.log('id',id)
+    db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            console.log(entries)
+            return res.status(200).json(entries[0])
+        })
+        .catch(err => res.status(400).json("unable to fetch database record and update it"));
+}
+
+const handleApiCall = (req, res) => {
+    const reqBody = req.body
+    console.log('body: ', reqBody)
+    const raw = JSON.stringify({
+        "user_app_id": {
+          "user_id": "dqxfhjtprube",
+          "app_id": "e1ae4e7329f4445492f9ce03f49c12b3"
+        },
+        "inputs": [
+          {
+            "data": {
+              "image": {
+                "url": req.body.imageUrl
+              }
+            }
+          }
+        ]
+      });
+      
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key 1950b40831d2490698c7643aafd8b6cd'
+        },
+        body: raw
+      };
+      fetch("https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs", requestOptions)
+      .then(response => {
+          return res.status(200).json(response);
+        })
+      .catch(error => console.log('error', error));
+}
+
+module.exports = {
+    addUser,
+    signin,
+    getUserProfile,
+    addEntries,
+    handleApiCall,
 }
--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 const express = require('express');
 const cors = require('cors')
+const userController = require('./controllers/user.controller')
 const app = express();
-const bcrypt = require('bcrypt')
-const db = require('knex')({
-    client: 'pg',
-    connection: {
-      host : 'localhost',
-      port : 5432,
-      user : 'postgres',
-      password : 'test',
-      database : 'TestDB'
-    }
-  });
-
 const PORT = 5000;
 
 //express middleware json parser
@@ -21,19 +10,12 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cors())
 // app.use(bodyParser.json()) //deprecated 
 
-db.select('*').from('users').then(data => {
-    console.log(data)
-})
-
-
-
 //middleware request/response handler
 app.use((req,res,next) => {
     const start = Date.now();
     next(); 
     const delta = Date.now() - start; 
     console.log(`${req.method} ${req.url} ${delta}ms`)
-  
 })
 
 app.get('/', (req, res) => {
@@ -43,67 +25,18 @@ app.get('/', (req, res) => {
     })
 })
 //sigin => post == success/fail
-app.post('/signin', (req, res) => {
-    let found = false
-  database.users.forEach(user => {
-      if (req.body.email === user.email){
-          found = true
-          res.status(200).json('Success')
-      }
-  })
-  if (!found){
-    res.status(404).json('User Not Found')
-  }
-})
-
+app.post('/signin', (req, res) => {userController.signin(req,res)})
 ///register => post == new user object
-app.post('/register', (req,res) => {
-    //grab body from request and push to database
-    const {name, email, password} = req.body;
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        createdDate: new Date().toISOString()
-    }).then(user => {
-        res.json(user[0])
-    })
-    .catch(err => res.status(400).json("unable to add to database"))
-
-})
+app.post('/register',(req, res) => {userController.addUser(req,res)})
 
 ///profile/:userID => get == user object
-app.get('/profile/:userID', (req, res) => {
-    const { userID } = req.params;
-    let found = false
-    database.users.forEach(user => {
-        if (userID === user.id){
-            found = true
-            return res.status(200).json(user)
-        }
-    })
-    if (!found){
-        res.status(404).json('Not Found')
-    }
-})
+app.get('/profile/:userID', (req,res) => {userController.getUserProfile(req,res)})
 
 ///image => put ==
-app.put('/image', (req,res) => {
-    const { userID } = req.body;
-    let found = false
-    database.users.forEach(user => {
-        if (userID === user.id){
-            found = true
-            user.entries++
-            return res.status(200).json(user.entries)
-        }
-    })
-    if (!found){
-        res.status(404).json('Not Found')
-    }
-})
+app.put('/image', (req,res) => {userController.addEntries(req,res)})
 
-app.listen(PORT, () => { 
+app.post('/detectionUrl/', (req,res) => {userController.handleApiCall(req,res)})
+
+app.listen(PORT, () => {
     console.log(`Listening to Port ${PORT}........`)
 })   
